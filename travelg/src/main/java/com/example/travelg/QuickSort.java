e4917388fdf51d247aa2163683fe35ef100b8933
GeorKyrs <georkyrs@teiser.gr>
2018-12-02 22:07:02 +0200

    #21 Retrieve data from the server

--- travelg/src/main/java/com/example/travelg/QuickSort.java
+++ travelg/src/main/java/com/example/travelg/QuickSort.java
@@ -1,57 +1,40 @@
+
 package com.example.travelg;
 
 import java.util.Collections;
 import java.util.List;
 
-public class QuickSort {
 
-    public void sortDouble(List<Double> list, int start, int end)
-    {
-        int i = start;
-        int j = end;
+public class QuickSort<T extends Comparable<? super T>> {
 
-        if (j - i >= 1)
-        {
-            double pivotValue = list.get(start);
-            while (j > i)
-            {
-                while (list.get(i) <= pivotValue && i < end && j > i){
-                    i++;
-                }
-                while (list.get(j) >= pivotValue && j > start && j >= i){
-                    j--;
-                }
-                if (j > i)
-                    Collections.swap(list, i, j);
-            }
-            Collections.swap(list, start, j);
-            sortDouble(list, start, j - 1);
-            sortDouble(list, j + 1, end);
-        }
-    }
+    public void sort(List<T> list) {
 
-    public void sortInt(List<Integer> list, int start, int end)
-    {
-        int i = start;
-        int j = end;
+        Collections.sort(list, T::compareTo);
 
-        if (j - i >= 1)
-        {
-            int pivotValue = list.get(start);
-            while (j > i)
-            {
-                while (list.get(i) <= pivotValue && i < end && j > i){
-                    i++;
-                }
-                while (list.get(j) >= pivotValue && j > start && j >= i){
-                    j--;
-                }
-                if (j > i)
-                    Collections.swap(list,i,j);
-            }
-            Collections.swap(list, start, j);
-            sortInt(list, start, j - 1);
-            sortInt(list, j + 1, end);
-        }
     }
 }
+
+//    public void sort(List<T> list, int start, int end)
+//    {
+//        int i = start;
+//        int j = end;
+//
+//        if (j - i >= 1)
+//        {
+//            T pivotValue = list.get(start);
+//            while (j > i)
+//            {
+//                while (list.get(i).compareTo(pivotValue)<=0 && i<end && j > i){
+//                    i++;
+//                }
+//                while (list.get(j).compareTo(pivotValue)>=0 && j > start && j >= i){
+//                    j--;
+//                }
+//                if (j > i)
+//                    Collections.swap(list,i,j);
+//            }
+//            Collections.swap(list, start, j);
+//            sort(list, start, j - 1);
+//            sort(list, j + 1, end);
+//        }
+//    }
