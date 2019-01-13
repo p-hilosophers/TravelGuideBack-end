63cebc47b296fe269de14a206a54607ffb0073ca
GeorKyrs <georkyrs@teiser.gr>
2019-01-13 19:02:47 +0200

    #21 Retrieve data from the server

--- travelg/src/main/java/com/example/travelg/JsonDataParser.java
+++ travelg/src/main/java/com/example/travelg/JsonDataParser.java
@@ -1,21 +1,31 @@
 package com.example.travelg;
 
+import com.example.travelg.Model.Sight;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
 public class JsonDataParser {
 
-    private List<String> destAddresses = new ArrayList<>(),distanceInKm = new ArrayList<>(),durationInMin = new ArrayList<>();
-    private List<Double> distanceValue = new ArrayList<>();
+    private List<String> distanceInKm = new ArrayList<>(),durationInMin = new ArrayList<>();
+    private List<Double> destinationsLat = new ArrayList<>(), destinationsLon = new ArrayList<>();
     private List<SightDto> list = new ArrayList<>();
+    private List<Sight> sights;
 
-@GetMapping("/sortByDistance/{cityName}")
-    public void dataParsing(double originsLat, double originsLon, List<Double> destinationsLat, List<Double> destinationsLon) {
+@GetMapping("/calculateDistance/{originsLat}/{originsLon}/{cityName}")
+    public List<SightDto> dataParsing(@PathVariable double originsLat,@PathVariable double originsLon, @PathVariable String cityName) throws IOException {
+        sights = SightDataRetriever.returnData(cityName);
+        for (int i = 0; i < SightDataRetriever.getCount(); i++) {
+            destinationsLat.add(sights.get(i).getLatitude());
+            destinationsLon.add(sights.get(i).getLongitude());
+        }
         JSONObject obj ;
         obj = DistanceCalculator.calcDistance(originsLat, originsLon, destinationsLat, destinationsLon);
         System.out.println(obj.toString());
@@ -29,10 +39,9 @@ public class JsonDataParser {
                 list.add(sDto);
            }
         }
-    }
+    list.sort(Comparator.comparing(SightDto::getDistance));
 
-    public List<String> getDestAddresses() {
-        return destAddresses;
+    return list;
     }
 
     public List<String> getDistanceInKm() {
@@ -41,9 +50,5 @@ public class JsonDataParser {
 
     public List<String> getDurationInMin() {
         return durationInMin;
-    }
-
-    public List<Double> getDistanceValue() {
-        return distanceValue;
     }
 }
